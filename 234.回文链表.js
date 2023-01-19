a0841a9f275f3fb9aa4ea249ea7d175992b8b525
KKPROGRAMING <2844359985@qq.com>
2023-01-19 15:10:31 +0800

    runtime beats 11 %;memory usage beats 60.37 %

---
+++ 234.回文链表.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=javascript
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let res = "";
+    while(head!==null) {
+        res += head.val;
+        head = head.next;
+    }
+    return res===[...res].reverse().join("");
+};
+// @lc code=end
+
+
