242690bc1fc6b77189a1d70cc7a608f1b93c8dfe
KKPROGRAMING <2844359985@qq.com>
2023-01-20 15:12:40 +0800

    runtime beats 21.83 %;memory usage beats 55.23 %

---
+++ 303.区域和检索-数组不可变.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    let sum = 0;
+    while(left<=right) {
+        sum += this.nums[left];
+        left += 1;
+    }
+    return sum;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+// @lc code=end
+
