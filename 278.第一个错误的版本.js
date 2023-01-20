ee37eb0402178569b26902e3e8b10e89a3583c22
KKPROGRAMING <2844359985@qq.com>
2023-01-20 13:37:43 +0800

    runtime beats 5.78 %;memory usage beats 97.94 %

---
+++ 278.第一个错误的版本.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=javascript
+ *
+ * [278] 第一个错误的版本
+ */
+
+// @lc code=start
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        while(n>=1) {
+            if(!isBadVersion(n)) {
+                return n+1;
+            }else {
+                n -= 1;
+            }
+        }
+        return 1;
+    };
+};
+// @lc code=end
+
